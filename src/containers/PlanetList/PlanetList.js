f38043f0faa0158e72002f5f1cf466d1eb02bf6d
Diego T. Fialho <diego.tfialho@gmail.com>
2018-11-16 10:00:58 -0200

    Paginação e mensagem de erro adicionadas na listagem de planetas

--- src/containers/PlanetList/PlanetList.js
+++ src/containers/PlanetList/PlanetList.js
@@ -15,20 +15,66 @@ export class PlanetList extends Component {
     this.props.selectPlanet(planet);
   }
 
+  previousPage = () => {
+    this.props.fetchPlanets(this.props.previous);
+  }
+
+  nextPage = () => {
+    this.props.fetchPlanets(this.props.next);
+  }
+
   render() {
     return (
       <Fragment>
 
         <section className="PlanetList">
-          {this.props.planets.map(planet => (
-            <Planet
-              planet={planet}
-              key={planet.name}
-              click={() => this.selectPlanet(planet)} />
-          ))}
+
+          {
+            this.props.planets.length
+              ? this.props.planets.map(planet => (
+                <Planet
+                  planet={planet}
+                  key={planet.name}
+                  click={() => this.selectPlanet(planet)} />
+              ))
+              : (
+                <div className="NoData">
+
+                  <h1>
+                    Oops!<br />
+                    Sorry, looks like something went wrong.<br />
+                    We already sent a team of robots to solve the problem.<br />
+                    Please try again later.
+                  </h1>
+
+                </div>
+              )
+          }
+
+          <div className="PageControls">
+
+            <button
+              type="button"
+              onClick={() => this.previousPage()}
+              className="Prev"
+              disabled={!this.props.previous}>
+              Prev
+            </button>
+
+            <button
+              type="button"
+              onClick={() => this.nextPage()}
+              className="Next"
+              disabled={!this.props.next}>
+              Next
+            </button>
+
+          </div>
+
         </section>
 
         <PlanetSelected />
+
       </Fragment>
     );
   }
@@ -36,6 +82,8 @@ export class PlanetList extends Component {
 
 PlanetList.propTypes = {
   planets: PropTypes.array,
+  previous: PropTypes.string,
+  next: PropTypes.string,
   fetchPlanets: PropTypes.func,
   selectPlanet: PropTypes.func,
 };
