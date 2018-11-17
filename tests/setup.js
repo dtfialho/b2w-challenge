2429d1beb06a7ddadf7511b03faa50bf0bc2df2a
Diego T. Fialho <diego.tfialho@gmail.com>
2018-11-17 14:31:18 -0200

    Testes unitários do PlanetList container

--- tests/setup.js
+++ tests/setup.js
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+const scrollToSpy = jest.fn();
+global.scrollTo = scrollToSpy;
